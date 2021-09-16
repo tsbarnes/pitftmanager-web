d5ed195c62e0f31b1688982e5723e2f4e0741eab
Thea Barnes <thea@tsbarnes.com>
2021-09-15 23:05:25 -0400

    More work

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import logging
 import posix_ipc
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect
 
 app = Flask(__name__)
 app.config.from_pyfile("app.cfg")
@@ -21,14 +21,49 @@ def index():
 def next():
     mq.send("next", timeout=10)
     flash("Sent 'next' message to epdtext")
-    return render_template('index.html')
+    return redirect('/')
 
 
 @app.route('/previous')
 def previous():
     mq.send("previous", timeout=10)
     flash("Sent 'previous' message to epdtext")
-    return render_template('index.html')
+    return redirect('/')
+
+
+@app.route('/button0')
+def button0():
+    mq.send("button0", timeout=10)
+    flash("Sent 'KEY1' message to epdtext")
+    return redirect('/')
+
+
+@app.route('/button1')
+def button1():
+    mq.send("button1", timeout=10)
+    flash("Sent 'KEY2' message to epdtext")
+    return redirect('/')
+
+
+@app.route('/button2')
+def button2():
+    mq.send("button2", timeout=10)
+    flash("Sent 'KEY3' message to epdtext")
+    return redirect('/')
+
+
+@app.route('/button3')
+def button3():
+    mq.send("button3", timeout=10)
+    flash("Sent 'KEY4' message to epdtext")
+    return redirect('/')
+
+
+@app.route('/reload')
+def reload():
+    mq.send("reload", timeout=10)
+    flash("Sent 'reload' message to epdtext")
+    return redirect('/')
 
 
 if __name__ == '__main__':
